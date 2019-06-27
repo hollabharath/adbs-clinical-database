205aa977bb6408bea02686a619e61e86f514d81b
Bharath Holla <45445206+hollabharath@users.noreply.github.com>
2019-06-27 14:11:49 +0530

    Create HAMA_Scoring_ADBS.R

---
+++ R/HAMA_Scoring_ADBS.R
@@ -0,0 +1,21 @@
+df <- read.csv("/Users/hollabharath/Documents/Projects/ADBS/DATA_QC/Scripts/hamiltonanxietyratingscale_table.csv", header = T)
+
+# Check for subjects with more than 25% missing data/non response for exclusion 
+# Also exclude subjects with incorrect visit ID
+df_excluded <- df[rowSums(is.na(df)) > (ncol(df)-6)/4 , ]
+df_duplicates <- df[nchar(df$assessmentId) !=9 | df$subjectID >200000, ]
+df <- df[rowSums(is.na(df)) < (ncol(df)-6)/4 & nchar(df$assessmentId) ==9 & df$subjectID <200000, ]
+
+paste("HAMA")
+paste("Number of subjects with >25% missing data:", nrow(df_excluded))
+paste("Number of subjects with incorrect subject/visit ID:", nrow(df_duplicates))
+paste("Number of subjects with usable data:", nrow(df))
+
+# Visualize missing data
+VIM::aggr(df[,5:(ncol(df)-3)])
+
+# Calculate total HAMA score
+df$HAMA.score <- rowSums(df[,5:(ncol(df)-3)], na.rm = TRUE)
+
+# Save to file
+write.csv(df, "/Users/hollabharath/Documents/Projects/ADBS/HAMA.csv")
