29799225cedff3c91e0d68b1c63c617cbe0457e0
Bharath Holla <45445206+hollabharath@users.noreply.github.com>
2019-06-27 16:49:06 +0530

    Update HAMD_Scoring_ADBS.R

--- R/HAMD_Scoring_ADBS.R
+++ R/HAMD_Scoring_ADBS.R
@@ -1,10 +1,19 @@
-df <- read.csv("/Users/hollabharath/Documents/Projects/ADBS/DATA_QC/Scripts/hamiltonratingscalefordepression_table.csv", header = T)
+df <- read.csv("/Users/hollabharath/Documents/Projects/ADBS/DATA_QC/Scripts/hamiltonratingscalefordepression_table.csv", 
+               header = T)
 
 # Check for subjects with more than 25% missing data/non response for exclusion 
-# Also exclude subjects with incorrect visit ID
 df_excluded <- df[rowSums(is.na(df)) > (ncol(df)-6)/4 , ]
-df_duplicates <- df[nchar(df$assessmentId) !=9 | df$subjectID >200000, ]
-df <- df[rowSums(is.na(df)) < (ncol(df)-6)/4 & nchar(df$assessmentId) ==9 & df$subjectID <200000, ]
+
+# Also check for subjects with incorrect subject/visit ID
+df_duplicates <- df[nchar(df$assessmentId) !=9 | 
+                    df$subjectID >200000 | 
+                    duplicated(df$assessmentId, fromLast = T), ]
+
+# Make a clean data frame after removing the rows from above two groups
+df <- df[rowSums(is.na(df)) < (ncol(df)-6)/4 & 
+         nchar(df$assessmentId) ==9 & 
+         df$subjectID <200000 & 
+         !duplicated(df$assessmentId, fromLast = T), ]
 
 paste("HAMD")
 paste("Number of subjects with >25% missing data:", nrow(df_excluded))
@@ -14,7 +23,7 @@ paste("Number of subjects with usable data:", nrow(df))
 # Visualize missing data
 VIM::aggr(df[,5:(ncol(df)-3)])
 
-# Calculate total HAMA score
+# Calculate total HAMD score
 df$HAMD.score <- rowSums(df[,5:(ncol(df)-3)], na.rm = TRUE)
 
 # Save to file
