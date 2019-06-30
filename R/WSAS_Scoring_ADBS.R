2a44db078195c9c43afcf73731f6dd2d4f9e1f77
Bharath Holla <45445206+hollabharath@users.noreply.github.com>
2019-06-30 18:05:28 +0530

    Create WSAS_Scoring_ADBS.R

---
+++ R/WSAS_Scoring_ADBS.R
@@ -0,0 +1,33 @@
+df <- read.csv("/Users/hollabharath/Documents/Projects/ADBS/DATA_QC/Scripts/workandsocial_table.csv", header =T)
+
+names(df) <- c("id", "subjectID", "assessmentId", "test_taken_by", 
+               "retired", "work", "home", "social", "private", "relationships",
+               "deviceid", "date_of_last_update", "total")
+
+# Check for subjects with missing data/non response for exclusion 
+df_excluded <- df[is.na(df$total), ]
+
+# Also check for subjects with incorrect subject/visit ID
+df_duplicates <- df[nchar(df$assessmentId) !=9 | 
+                      df$subjectID >200000 | 
+                      duplicated(df$assessmentId, fromLast = T), ]
+
+# Make a clean data frame after removing the rows from above two groups
+df <- df[!is.na(df$total) & 
+           nchar(df$assessmentId) ==9 & 
+           df$subjectID <200000 & 
+           !duplicated(df$assessmentId, fromLast = T), ]
+
+# Visualize missing data
+VIM::aggr(df)
+
+df$WSAS_total <- rowSums(df[,c( "work", "home", "social", "private", "relationships")], na.rm = T)             
+
+paste("WSAS")
+paste("Number of subjects with complete missing data:", nrow(df_excluded))
+paste("Number of subjects with incorrect subject/visit ID:", nrow(df_duplicates))
+paste("Number of subjects with usable data:", nrow(df))
+
+
+# Save to file
+write.csv(df, "/Users/hollabharath/Documents/Projects/ADBS/WSAS.csv")
